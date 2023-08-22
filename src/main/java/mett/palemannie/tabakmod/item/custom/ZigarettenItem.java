93e5cba32a473c4a0ba4dd62195885a67a27dad6
PaleMannie <einalex44@gmail.com>
2023-08-22 21:24:48 +0200

    Tabakpflanze brennt jetzt, Items werden studiert um Tabakprodukte zu machen

---
+++ src/main/java/mett/palemannie/tabakmod/item/custom/ZigarettenItem.java
@@ -0,0 +1,10 @@
+package mett.palemannie.tabakmod.item.custom;
+
+
+import net.minecraft.world.item.Item;
+
+public class ZigarettenItem extends Item {
+    public ZigarettenItem(Properties pProperties) { super(pProperties); }
+
+
+}
