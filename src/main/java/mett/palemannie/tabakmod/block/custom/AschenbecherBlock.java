cebff4fd21ab04b8dcbcf687c9820e8df8ded656
PaleMannie <einalex44@gmail.com>
2023-09-17 00:48:54 +0200

    commit self funeral

---
+++ src/main/java/mett/palemannie/tabakmod/block/custom/AschenbecherBlock.java
@@ -0,0 +1,10 @@
+package mett.palemannie.tabakmod.block.custom;
+
+import net.minecraft.world.item.Item;
+
+public class AschenbecherBlock extends Item {
+    public AschenbecherBlock(Properties pProperties) {
+        super(pProperties);
+    }
+
+}
